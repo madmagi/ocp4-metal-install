4fcb76db6854ccd4f982d33d400ee35b3e6dc353
Lester Henderson <madmagi@gmail.com>
2022-06-20 13:35:06 -0600

    added virsh commands to create drives and machines

---
+++ harddrives/createdrives.sh
@@ -0,0 +1,9 @@
+#\bin\sh
+qemu-img create -f raw /var/lib/libvirt/images/ocp-bootstrap.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-cp-1.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-cp-2.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-cp-3.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-w-1.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-w-2.qcow2 50G
+qemu-img create -f raw /var/lib/libvirt/images/ocp-svc.qcow2 150G
+
---
+++ machines/createmachines.sh
@@ -0,0 +1,8 @@
+#\bin\sh
+virsh create ocp-bootstrap.xml
+virsh create ocp-cp-1.xml
+virsh create ocp-cp-2.xml
+virsh create ocp-cp-3.xml
+virsh create ocp-w-1.xml
+virsh create ocp-w-2.xml
+virsh create ocp-svc.xml
