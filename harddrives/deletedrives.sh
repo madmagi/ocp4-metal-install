25bd5b2717f4476efac7f1743f6c3b6a9779a754
Lester Henderson <madmagi@gmail.com>
2022-06-20 14:11:41 -0600

    fix bad edits

---
+++ harddrives/deletedrives.sh
@@ -0,0 +1,9 @@
+#\bin\sh
+rm /var/lib/libvirt/images/ocp-bootstrap.qcow2
+rm /var/lib/libvirt/images/ocp-cp-1.qcow2
+rm /var/lib/libvirt/images/ocp-cp-2.qcow2
+rm /var/lib/libvirt/images/ocp-cp-3.qcow2
+rm /var/lib/libvirt/images/ocp-w-1.qcow2
+rm /var/lib/libvirt/images/ocp-w-2.qcow2
+rm /var/lib/libvirt/images/ocp-svc.qcow2
+
