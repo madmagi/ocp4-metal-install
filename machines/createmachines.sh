dccbc7ed9aed6f96f3b49e3fa58df6deb60461af
Lester Henderson <madmagi@gmail.com>
2022-07-12 13:29:01 -0600

    fix create/destroy scripts

--- machines/createmachines.sh
+++ machines/createmachines.sh
@@ -1,8 +1,8 @@
 #\bin\sh
-virsh create ocp-bootstrap.xml
-virsh create ocp-cp-1.xml
-virsh create ocp-cp-2.xml
-virsh create ocp-cp-3.xml
-virsh create ocp-w-1.xml
-virsh create ocp-w-2.xml
-virsh create ocp-svc.xml
+virsh define ocp-bootstrap.xml
+virsh define ocp-cp-1.xml
+virsh define ocp-cp-2.xml
+virsh define ocp-cp-3.xml
+virsh define ocp-w-1.xml
+virsh define ocp-w-2.xml
+virsh define ocp-svc.xml
---
+++ machines/deletemachines.sh
@@ -0,0 +1,8 @@
+#\bin\sh
+virsh destroy ocp-bootstrap.xml
+virsh destroy ocp-cp-1.xml
+virsh destroy ocp-cp-2.xml
+virsh destroy ocp-cp-3.xml
+virsh destroy ocp-w-1.xml
+virsh destroy ocp-w-2.xml
+virsh destroy ocp-svc.xml
