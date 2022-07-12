5ae80017b433c3668ba05cf9d694e27e3c881269
Lester Henderson <madmagi@gmail.com>
2022-07-12 13:30:03 -0600

    remove xml postfix

--- machines/deletemachines.sh
+++ machines/deletemachines.sh
@@ -1,8 +1,8 @@
 #\bin\sh
-virsh destroy ocp-bootstrap.xml
-virsh destroy ocp-cp-1.xml
-virsh destroy ocp-cp-2.xml
-virsh destroy ocp-cp-3.xml
-virsh destroy ocp-w-1.xml
-virsh destroy ocp-w-2.xml
-virsh destroy ocp-svc.xml
+virsh destroy ocp-bootstrap
+virsh destroy ocp-cp-1
+virsh destroy ocp-cp-2
+virsh destroy ocp-cp-3
+virsh destroy ocp-w-1
+virsh destroy ocp-w-2
+virsh destroy ocp-svc
